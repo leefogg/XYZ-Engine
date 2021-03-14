a3c6fb825e97edaec1c9bcb51b5ee7c83319fd95
Lee Fogg <leefogg@users.noreply.github.com>
2021-03-14 18:58:36 +0000

    Splified Hello World example

--- GLOOP/Tests/Hello.cs
+++ GLOOP/Tests/Hello.cs
@@ -1,26 +1,15 @@
-﻿using GLOOP.Extensions;
-using GLOOP.Rendering;
-using OpenTK.Graphics.OpenGL4;
+﻿using GLOOP.Rendering;
 using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
-using OpenTK.Windowing.GraphicsLibraryFramework;
-using System;
-using System.Collections.Generic;
 using System.ComponentModel;
-using System.Text;
-using static OpenTK.Graphics.OpenGL4.GL;
 
 namespace GLOOP.Tests
 {
     public class Hello : Window
     {
         private DebugCamera Camera;
-        private Entity Model1;
-        private Entity Model2;
-        private Entity Plane;
-        private SingleColorMaterial singleColorMaterial;
-        private Vector3 sincos = new Vector3();
+        private Entity duck;
 
         public Hello(GameWindowSettings gameWindowSettings, NativeWindowSettings nativeWindowSettings) 
             : base(gameWindowSettings, nativeWindowSettings)
@@ -32,31 +21,12 @@ namespace GLOOP.Tests
         {
             base.OnLoad();
 
-            new Texture2D("assets/textures/black.png", PixelInternalFormat.Rgb);
-
             var assimp = new Assimp.AssimpContext();
             var shader = new FullbrightShader();
             var material = new FullbrightMaterial(shader);
-            Model1 = new Entity("assets/models/pyramid.dae", assimp, material);
-            Model1.Transform.Scale *= 10000f;
-            //Model1.Scale = new Vector3(0.2f, 0.4f, 0.9f);
-            Model1.Transform.Position.X = -1.5f;
-            Model1.Transform.Position.Y = 0.5f;
-            Model1.Models[0].Material.SetTextures(TextureCache.Get("assets/textures/duck.bmp"), null, null, null);
-
-            Model2 = new Entity("assets/models/sphere.obj", assimp, material);
-            Model2.Transform.Position.X = 1.5f;
-            Model2.Transform.Scale *= 0.1f;
-            Model2.Models[0].Material.SetTextures(TextureCache.Get("assets/textures/duck.bmp"), null, null, null);
-
-            Plane = new Entity("assets/models/plane.dae", assimp, material);
-            Plane.Transform.Scale *= new Vector3(100000f, 1f, 100000f);
-            //Plane.Scale = new Vector3(1 / Plane.WorldScale.X, 1 / Plane.WorldScale.Y, 1 / Plane.WorldScale.Z);
-            Plane.Models[0].Material.SetTextures(TextureCache.Get("assets/textures/gray.png"), null, null, null);
-
-            var singleColorShader = new SingleColorShader();
-            singleColorMaterial = new SingleColorMaterial(singleColorShader);
-            singleColorMaterial.Color = new Vector4(1);
+            duck = new Entity("assets/models/duck.dae", assimp, material);
+            duck.Models[0].Transform.Scale *= 0.01f;
+            duck.Models[0].Material.SetTextures(TextureCache.Get("assets/textures/duck.bmp"), null, null, null);
         }
 
         public override void Render()
@@ -64,26 +34,7 @@ namespace GLOOP.Tests
             // Have to do this here until I do a Material class
             var projectionMatrix = Camera.ProjectionMatrix;
             var viewMatrix = Camera.ViewMatrix;
-            Model1.Render(projectionMatrix, viewMatrix);
-            //Model2.Render(projectionMatrix, viewMatrix);
-            Plane.Render(projectionMatrix, viewMatrix);
-
-            var rotation = Quaternion.FromAxisAngle(Vector3.UnitX, sincos.X) * Quaternion.FromAxisAngle(Vector3.UnitY, sincos.Y) * Quaternion.FromAxisAngle(Vector3.UnitZ, sincos.Z);
-            sincos += new Vector3(0.01f, 0.00f, 0.01f);
-            Model1.Transform.Rotation = rotation;
-            Model1.RenderBoundingBox(projectionMatrix, viewMatrix);
-
-            var verts = new List<Vector3>(8);
-            Model1.OriginalBoundingBox
-                .Translated(-Model1.OriginalBoundingBox.Center)
-                .Scaled(Model1.Transform.Scale, Vector3.Zero)
-                .GetCorners(verts);
-            verts.RotateAround(Model1.Transform.Rotation.Inverted(), Model1.OriginalBoundingBox.Center);
-            foreach (var vert in verts)
-            {
-                Model2.Transform.Position = vert + new Vector3(0,1,0);
-                Model2.Render(projectionMatrix, viewMatrix);
-            }
+            duck.Render(projectionMatrix, viewMatrix);
         }
 
         protected override void OnUpdateFrame(FrameEventArgs args)
