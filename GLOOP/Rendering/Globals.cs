767cf4a4c38e9a1cccf43c6dc21139ad9a78e7a5
Lee Fogg <leefogg@users.noreply.github.com>
2022-01-04 18:32:46 +0000

    Added inspection of supported extensions

--- GLOOP/Rendering/Globals.cs
+++ GLOOP/Rendering/Globals.cs
@@ -1,6 +1,7 @@
 ﻿using OpenTK.Graphics.OpenGL4;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace GLOOP.Rendering
@@ -9,6 +10,7 @@ namespace GLOOP.Rendering
     {
         private static int? maxLabelLength, uniformBufferOffsetAlignment, maxTextureUnits, maxDebugGroupNameLength;
         private static float? maxTextureAnisotropy;
+        private static string[]? supportedExtensions;
 
         public static int MaxLabelLength => maxLabelLength ??= GL.GetInteger((GetPName)All.MaxLabelLength);
 
@@ -19,5 +21,23 @@ namespace GLOOP.Rendering
         public static int MaxDebugGroupNameLength => maxDebugGroupNameLength ??= GL.GetInteger((GetPName)All.MaxDebugMessageLength);
 
         public static float MaxTextureAnisotropy => maxTextureAnisotropy ??= GL.GetFloat((GetPName)All.MaxTextureMaxAnisotropy);
+
+        public static string[] SupportedExtensions
+        {
+            get
+            {
+                if (supportedExtensions == null)
+                {
+                    var numExtensions = GL.GetInteger(GetPName.NumExtensions);
+                    supportedExtensions = new string[numExtensions];
+                    for (int i = 0; i < numExtensions; i++)
+                        supportedExtensions[i] = GL.GetString(StringNameIndexed.Extensions, i);
+                }
+
+                return supportedExtensions;
+            }
+        }
+
+        public static bool SupportsExtension(string extensionName) => SupportedExtensions.Contains(extensionName);
     }
 }
