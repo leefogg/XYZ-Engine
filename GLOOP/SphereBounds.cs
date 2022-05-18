506f6e416d1559a53f64b4e8a43637e147dc31fe
Lee Fogg <leefogg@users.noreply.github.com>
2022-05-18 19:34:01 +0100

    Sphere -> SphereBounds

--- GLOOP/SphereBounds.cs
+++ GLOOP/SphereBounds.cs
@@ -1,6 +1,8 @@
-﻿using OpenTK.Mathematics;
+﻿using GLOOP.Rendering;
+using OpenTK.Mathematics;
 using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 using System.Text;
 
 namespace GLOOP
@@ -16,10 +18,23 @@ namespace GLOOP
             Radius = radius;
         }
 
+        public SphereBounds(Box3 boundingBox, Transform objectTransform)
+        {
+            Position = boundingBox.Center + objectTransform.Position;
+            var size = boundingBox.Size * objectTransform.Scale;
+            Radius = Math.Max(Math.Max(size.X, size.Y), size.Z) / 2f;
+        }
+
         public void Deconstruct(out Vector3 position, out float radius)
         {
             position = Position;
             radius = Radius;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static bool IntersectsPlane(Vector4 planeEquation, Vector3 Position, float Radius)
+        {
+            return planeEquation.X * Position.X + planeEquation.Y * Position.Y + planeEquation.Z * Position.Z + planeEquation.W <= -Radius;
         }
     }
 }
